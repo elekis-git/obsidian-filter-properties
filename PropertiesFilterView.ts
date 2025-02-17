85066dd938cd73b28173d3bb25e45172295e5ec0
= <btlc@pm.me>
2025-02-17 14:34:49 +0100

    Premier Commit

---
+++ PropertiesFilterView.ts
@@ -0,0 +1,212 @@
+import { ItemView, WorkspaceLeaf, TFile } from 'obsidian';
+
+
+
+export class PropertiesFilterView extends ItemView {
+
+    public static VIEW_TYPE_PROPERTIES_FILTER = 'propertiesfilterelekis';
+
+
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+  }
+ 
+
+  getViewType(): string {
+    return PropertiesFilterView.VIEW_TYPE_PROPERTIES_FILTER;
+  }
+
+  getDisplayText(): string {
+    return 'Properties Filter Plugin';
+  }
+
+  getIcon(): string {
+    return "filter"; 
+}
+
+
+  async onOpen() {
+
+
+    const container = this.containerEl;
+    const windowContainer = container.createDiv({ cls: 'properties-filter-elekis-container' });
+
+    // Fenêtre 1 : Propriétés de tous les fichiers
+    const window1 = windowContainer.createDiv({ cls: 'properties-filter-elekis-window1' });
+    const buttondiv = window1.createEl('div', { cls: 'properties-filter-elekis-add-line' });
+    const filterdiv = window1.createEl('div', { cls: 'properties-filter-elekis-filtered' });
+    const buttonContainer = buttondiv.createDiv({ cls: 'properties-filter-elekis-button-container' });
+
+    const newSelectElement      = buttonContainer.createEl('select', { cls: 'properties-filter-elekis-select' });
+    const newValueSelectElement = buttonContainer.createEl('select', { cls: 'properties-filter-elekis-select' });
+    const addFilterButton       = buttonContainer.createEl('button', { text: '+', cls: 'properties-filter-elekis-button-add' });
+    const addResetButton        = buttonContainer.createEl('button', { text: 'R', cls: 'properties-filter-elekis-button-reset' });
+
+    newSelectElement.createEl('option', { text: 'all', value: 'all' });
+    newValueSelectElement.createEl('option', { text: 'all', value: 'all' });
+
+    let userFilter = [{ key: 'all', value: 'all'}]  //les filtres actifs.
+    let fileFiltered: TFile[] = []  // visible dans windows 2
+    let fileMetadata: { [filePath: string]: any } = {};
+    let keyValuesMap: { [key: string]: string[] } = {};
+    fileFiltered = this.app.vault.getFiles();
+    
+
+    const getmetadataFile = (files: TFile[]) => {   
+        const allKeys: string[] = ['all'];
+        newSelectElement.empty()
+        // Récupérer toutes les propriétés et valeurs dans les fichiers
+        files.forEach((file: TFile) => {
+            const metadata = this.app.metadataCache.getFileCache(file);
+            if (metadata?.frontmatter) {
+                for (const [key, value] of Object.entries(metadata.frontmatter)) {
+                    if (!allKeys.includes(key)) allKeys.push(key);
+                    if (!keyValuesMap[key]) keyValuesMap[key] = [];
+                    if (!keyValuesMap[key].includes(String(value))) keyValuesMap[key].push(String(value));
+                }
+                fileMetadata[file.path] = metadata.frontmatter;
+            }
+        });
+        const sortedKeys = ['all', ...allKeys.slice(1).sort((a, b) => a.localeCompare(b))];
+        sortedKeys.forEach((key) => {
+            newSelectElement.createEl('option', { text: String(key), value: String(key) });
+      });
+    };
+
+    getmetadataFile(fileFiltered);
+
+      // Mettre à jour les valeurs quand une nouvelle propriété est choisie
+      const updateNewValueList = (selectedKey: string) => {
+        newValueSelectElement.empty();
+        newValueSelectElement.createEl('option', { text: 'all', value: 'all' });
+
+        if (selectedKey && keyValuesMap[selectedKey]) {
+          keyValuesMap[selectedKey].forEach((value) => {
+            newValueSelectElement.createEl('option', { text: value, value: value });
+          });
+        }
+      };
+
+    addFilterButton.addEventListener('click', () => {
+        const properText = newSelectElement.options[newSelectElement.selectedIndex].text;
+        const valueText = newValueSelectElement.options[newValueSelectElement.selectedIndex].text;
+        const filterdivd        = filterdiv.createEl('div', {cls: 'properties-filter-elekis-divprop' });
+        const filterdivt        = filterdivd.createEl('h5', { text: properText+":"+valueText }); 
+        const filterdivb        = filterdivd.createEl('button', { text: '-', cls: 'properties-filter-elekis-divprop-button' });
+        filterdivd.dataset.key = properText;
+        filterdivd.dataset.value = valueText;
+
+        filterdivb.addEventListener('click', () => {
+               console.log(userFilter);
+               console.log(fileFiltered);
+               userFilter = userFilter.filter(filter => !(filter.key === filterdivd.dataset.key && filter.value === filterdivd.dataset.value));
+               userFilter.remove({ key: properText, value: valueText}) 
+               filterdivd.remove()
+               fileFiltered = this.app.vault.getFiles();
+               getmetadataFile(fileFiltered);
+               updateFileList(userFilter);
+               updateAddFilterButtonState();
+                });
+         userFilter.push({ key: properText, value: valueText})
+         updateFileList(userFilter);        
+         newSelectElement.selectedIndex = 0; 
+         fileMetadata = {}; 
+         keyValuesMap = {};
+         getmetadataFile(fileFiltered); 
+         updateNewValueList('all');
+         updateAddFilterButtonState();
+        
+    });
+
+    addResetButton.addEventListener('click', () => {
+        // Vider le tableau
+        filterdiv.empty();
+        // Mettre à jour la liste des fichiers après le reset
+        userFilter = [{ key: 'all', value: 'all'}]
+        fileFiltered = this.app.vault.getFiles();
+        fileMetadata = {}; 
+        keyValuesMap = {};
+        updateFileList(userFilter);
+        updateNewValueList('all');
+        getmetadataFile(fileFiltered);
+        });
+
+  // Event listeners pour les nouvelles lignes de filtres
+      newSelectElement.addEventListener('change', (event) => {
+        const selectedKey = (event.target as HTMLSelectElement).value;
+        updateNewValueList(selectedKey);
+        updateAddFilterButtonState();
+      });
+ 
+     newValueSelectElement.addEventListener('change', () => {
+        updateAddFilterButtonState(); // Met à jour l'état du bouton
+        });
+
+    // Fenêtre 2 : Liste des fichiers filtrés
+    const window2 = windowContainer.createDiv({ cls: 'properties-filter-elekis-window2' });
+
+    window2.createEl('h2', { text: 'files' });
+    const fileList = window2.createEl('ul', { cls: 'properties-filter-elekis-file-list' });
+
+
+    const updateFileList = (filters: { key: string; value: string }[]) => {
+            fileList.empty();
+
+            // Filtrer uniquement les fichiers qui passent tous les filtres
+            fileFiltered = fileFiltered.filter((file: TFile) => {
+                const metadata = fileMetadata[file.path];
+
+                return filters.every(filter => {
+                    if (filter.key === 'all') return true; // Si "all", ne pas filtrer
+                    if (typeof metadata === 'undefined') return false;
+                    if (filter.value === 'all' && filter.key in metadata) return true;
+                    return String(metadata[filter.key]) === filter.value;
+                });
+            });
+
+            // Maintenant, fileFiltered ne contient que les fichiers valides
+            fileFiltered.forEach((file: TFile) => {
+                const fileItem = fileList.createEl('li');
+                const fileLink = fileItem.createEl('a', {
+                    href: '',
+                    text: file.name,
+                    cls: 'properties-filter-elekis-file-link',
+                });
+
+                // Ouvrir le fichier lors du clic sur un élément de la liste
+                fileLink.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    this.app.workspace.openLinkText(file.path, '', true);
+                });
+            });
+};
+
+            const updateAddFilterButtonState = () => {
+                const selectedKey = newSelectElement.value;
+                const selectedValue = newValueSelectElement.value;
+
+                if (selectedKey === 'all' && selectedValue === 'all') {
+                    addFilterButton.disabled = true;
+                    addFilterButton.style.backgroundColor = 'gray'; // Couleur désactivée
+                    addFilterButton.style.cursor = 'not-allowed';
+                } else {
+                    addFilterButton.disabled = false;
+                    addFilterButton.style.backgroundColor = ''; // Réinitialiser la couleur
+                    addFilterButton.style.cursor = 'pointer';
+                }
+            };
+
+
+        // Afficher tous les fichiers au début
+    userFilter = [{ key: 'all', value: 'all'}]
+    updateFileList(userFilter);
+    updateAddFilterButtonState();
+    // Styles pour les liens des fichiers
+    const fileLinks = fileList.querySelectorAll('.properties-filter-elekis-file-link');
+    fileLinks.forEach((link) => {
+    });
+  }
+async onClose(): Promise<void>{
+    // Logique pour la fermeture de la vue
+  }
+}
---
+++ main.ts
@@ -0,0 +1,62 @@
+import { Plugin, WorkspaceLeaf } from 'obsidian';
+import { PropertiesFilterView } from './PropertiesFilterView';
+
+// mettre une option pour savoir si on l'integre dans le filetree direct (un, ou les deux)'
+
+export default class PropertiesFilterPlugin extends Plugin {
+  
+  private static instance: PropertiesFilterPlugin | null = null;
+
+  
+  async onload() {
+    console.log("PropertiesFilter plugin loaded");
+
+      if (PropertiesFilterPlugin.instance) {
+      console.warn("PropertiesFilter is already loaded!");
+      return; 
+    }
+    PropertiesFilterPlugin.instance = this;
+
+    // Enregistrer la vue pour le plugin
+    this.registerView(
+      PropertiesFilterView.VIEW_TYPE_PROPERTIES_FILTER,
+      (leaf: WorkspaceLeaf) => new PropertiesFilterView(leaf)
+    );
+
+    console.log("Vue enregistrée :", PropertiesFilterView.VIEW_TYPE_PROPERTIES_FILTER);
+
+
+    // Ajouter un bouton dans le ribbon
+    this.addRibbonIcon('filter', 'PropertiesFilter', async () => {
+      // Ouvrir la vue dans le panneau de gauche
+      await this.activateView();
+    });
+  }
+
+
+
+async activateView() {
+    const { workspace } = this.app;
+
+    let leaf = workspace.getLeavesOfType(PropertiesFilterView.VIEW_TYPE_PROPERTIES_FILTER)[0];
+
+    if (!leaf) {
+        leaf = workspace.getLeftLeaf(false)!;
+        await leaf.setViewState({
+            type: PropertiesFilterView.VIEW_TYPE_PROPERTIES_FILTER,
+            active: true
+        });
+    }
+
+   
+
+    workspace.revealLeaf(leaf);
+}
+
+
+  onunload() {
+    console.log("PropertiesFilter plugin unloaded");
+    PropertiesFilterPlugin.instance = null;
+  }
+}
+
